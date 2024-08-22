60b8b24393f447ea4ba52b1b6f0885086d0543f9
Hager-Gomaa <hagargomaa313@gmail.com>
2024-08-22 19:43:28 +0300

    commit

--- composeApp/src/androidMain/kotlin/com/hager_gomaa/decomposepractise/screenone/ui/component/ImageRemoteItem.kt
+++ composeApp/src/androidMain/kotlin/com/hager_gomaa/decomposepractise/screenone/ui/component/ImageRemoteItem.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
@@ -27,6 +29,7 @@ fun ImageItem(item: ImageUiState) {
             modifier = Modifier
                 .fillMaxWidth() // make it take screen width
                 .height(300.dp) //make it take 300 dp in height
+                .clip(RoundedCornerShape(16.dp)) // make with corner radios
         )
     }
 }
