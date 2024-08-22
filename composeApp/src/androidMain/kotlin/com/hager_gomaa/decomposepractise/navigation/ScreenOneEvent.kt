946b401f370c459fbb7f92e46daf4557302accea
Hager-Gomaa <hagargomaa313@gmail.com>
2024-08-22 18:22:53 +0300

    commit

--- composeApp/src/androidMain/kotlin/com/hager_gomaa/decomposepractise/navigation/ScreenOneEvent.kt
+++ composeApp/src/androidMain/kotlin/com/hager_gomaa/decomposepractise/navigation/ScreenOneEvent.kt
@@ -1,6 +1,6 @@
 package com.hager_gomaa.decomposepractise.navigation
 
 // hande type of screen one events
-sealed interface ScreenOneEvent {
-    data object ClickButtonA : ScreenOneEvent
+sealed class ScreenOneEvent {
+    data object ShowMoreClicked : ScreenOneEvent()
 }
